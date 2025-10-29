b69c0c574c776fe0807117185902f996c956d04d
ethever <25705974+ethever@users.noreply.github.com>
2025-10-29 19:24:50 +0800

    run script

--- run_gigahorse.sh
+++ run_gigahorse.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Simple script to run gigahorse with the specified parameters
+./build_and_extract.sh
+# ./gigahorse.py examples/test.hex -C aa.dl --restart --debug
 
-./gigahorse.py examples/test.hex -C aa.dl --restart --debug
+./gigahorse.py examples/test.hex -C aa.dl --rerun_clients --debug
